11e5f1833dee09a4f2bf438d7a782f1a77eb0af2
nthokiesther09 <nthokiesther09@gmail.com>
2022-11-27 15:39:40 +0300

    completed grade generator

---
+++ grade.js
@@ -0,0 +1,27 @@
+// Defined prompt function
+
+const prompt = require("prompt-sync")();
+let grade = prompt('Enter Marks:');
+{
+//Categorising grade A,B,C,D,E
+
+if (grade <= 100 && grade >79){
+    console.log('A')
+}
+
+else if (grade <= 79 && grade >= 60 ){
+    console.log ('B')
+}
+
+else if (grade <= 59 && grade > 49){
+    console.log('C')
+}
+
+else if (grade <= 49 && grade >= 40 ){
+    console.log('D')
+}
+
+else if (grade < 40){
+    console.log('E')
+}
+};
